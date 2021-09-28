dc625f25d7ccfcb90f4d3b1896ac1891c63401d4
gilbertomoj <gibamedeirosgc@gmail.com>
2021-09-27 21:08:26 -0300

    Atividade

---
+++ 1/index.js
@@ -0,0 +1,21 @@
+//1
+// const dominio1 = 'mundojs'
+// const dominio2 = 'google'
+
+// console.log(`Olá ${dominio1}! O site www.${dominio2}.com te ajuda a achar muitos materiais de estudo`)
+
+//2 
+// var valorA = 'casa'
+// var valorB = 'asa'
+// valorA.toUpperCase()
+// valorB.toUpperCase()
+// console.log(valorA, valorB)
+// if(valorA == valorB){
+//     console.log(`${valorA} == ${valorB}`)
+// }else{
+//     console.log(`${valorA} != ${valorB}`)
+// }
+
+//3
+// const valorA = "substring(): Aprenda a utilizar."
+// console.log(valorA.substring(13,32))
